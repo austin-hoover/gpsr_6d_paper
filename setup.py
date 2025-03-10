60aca7068e5a3452fbde695fb02235a350f1afb1
austin-hoover <austin.michael.hoover@gmail.com>
2025-03-10 10:22:45 -0400

    Add phase_space_reconstruction.virtual as package

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ from setuptools import setup
 setup(
     name='phase_space_reconstruction',
     version='0.0.1',
-    packages=['phase_space_reconstruction', 'phase_space_reconstruction.beams'],
+    packages=[
+        'phase_space_reconstruction', 
+        'phase_space_reconstruction.beams',
+        'phase_space_reconstruction.virtual',
+    ],
     url='',
     license='',
     author='RyanRoussel',
